a063ae63f57e38d084b5a5b69758a31300f93d90
Dennis Rodriguez <dennisrodriguezx@gmail.com>
2023-01-13 17:46:30 -0400

    init

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="pokemon-container">
+        <img :src="imgSrc"
+            class="hidden-pokemon">
+        <img v-if="showPokemon" :src="imgSrc">
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed, toRef } from 'vue'
+
+interface Props {
+    pokemonId: number
+    showPokemon: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    pokemonId: 1,
+    showPokemon: false,
+})
+
+const pokemonId = toRef( props, 'pokemonId')
+
+const imgSrc = computed(() =>{
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ pokemonId.value }.svg`
+})
+</script>
+
+<style scoped>
+/* Pokemon Picture */
+.pokemon-container {
+    height: 200px;
+}
+img {
+    height: 200px;
+    position: absolute;
+    right: 32%;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+}
+.hidden-pokemon {
+    filter: brightness(0);
+}
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="container">
+        <h1>¿Quien es este Pokemon?</h1>
+
+        <h3 v-if="!pokemon" >Espere por favor...</h3>
+        <div v-else>
+            <PokemonPicture :pokemon-id="pokemon.id" :show-pokemon="showPokemon" />
+            <PokemonOptions :pokemons="pokemonArr" @selection="checkAnswer(/*1,*/ $event)"/>
+
+            <template v-if="showAnswer" class="fade-in">
+                <h2>{{ message }}</h2>
+                <button @click="newGame" >Nuevo juego</button>
+            </template>
+
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import PokemonPicture from '../components/PokemonPicture.vue'
+import PokemonOptions from '../components/PokemonOptions.vue'
+import getPokemonsOptions from '../helpers/getPokemonOptions'
+import { Pokemon } from '../interfaces/Pokemon';
+
+const pokemonArr = ref<Pokemon[]>([])
+const pokemon = ref<Pokemon>()
+const showPokemon = ref<boolean>(false)
+const showAnswer = ref<boolean>(false)
+const message = ref<string>('')
+
+const mixPokemonArray = async() => {
+    pokemonArr.value = await getPokemonsOptions()
+    // console.table(JSON.parse(JSON.stringify(pokemonArr.value)))
+
+    const rndInt = Math.floor( Math.random() * 4 )
+    pokemon.value = pokemonArr.value[ rndInt ]
+}
+
+onMounted(() => {
+    mixPokemonArray()
+})
+
+const checkAnswer = ( /*numero,*/ pokemonId: number) => {
+    showPokemon.value = true
+    showAnswer.value = true
+    if ( pokemonId === pokemon.value?.id ) {
+        message.value = `Correcto is ${ pokemon.value.name }!!`
+    } else {
+        message.value = `Oops was ${ pokemon.value!.name }!!`
+    }
+
+}
+
+const newGame = () => {
+    pokemonArr.value = []
+    pokemon.value = undefined
+    showPokemon.value = false
+    showAnswer.value = false
+    message.value = ''
+    mixPokemonArray()
+}
+</script>
+
+<style scoped>
+.container {
+    display: grid;
+}
+</style>
